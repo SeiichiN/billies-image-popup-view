fa1268b1cd157c67e1d9cd0411c822210de69d9d
SeiichiN <billie175@gmail.com>
2021-08-06 22:16:02 +0900

    jqueryに変更

--- popup_view.js
+++ popup_view.js
@@ -2,76 +2,29 @@
 
 'use strict';
 
-(function () {
+$( () => {
   var i, imgs, imgcap,
       eles, imageLargeEle, targetFigcap, targetImg,
       setClick, image_area, body_area, setImageClick;
 
-  body_area = document.getElementsByTagName('body')[0];
-  image_area = document.getElementsByClassName('image-large-area')[0];
-  eles = document.getElementsByClassName('blocks-gallery-item');
-  imageLargeEle = document.getElementById('image-large');
-  targetFigcap = document.getElementById('figcap');
-  targetImg = document.createElement('img'); // img要素を作成
+  const html_txt = String()
+             + '<div class="image-large-area">'
+             +   '<div class="image-area-bg">'
+             +     '<figure id="image-large">'
+             +       '<figcaption id="figcap"></figcaption>'
+             +     '</figure>'
+             +   '</div>'
+             + '</div>'
 
-  setImageClick = function() {
-    imageLargeEle.addEventListener('click', function () {
-      
-      image_area.style.display = 'none';
-      body_area.style.backgroundColor = '#fff';
-    }, false);
-  };
+  $('.blocks-gallery-item').on('click', (e) => {
+    $('body').prepend(html_txt)
+    const img_src = e.target.src
+    const img_alt = e.target.alt
+    const img_html = '<img src="' + img_src + '" alt="' + img_alt + '">'
+    $('#image-large').prepend(img_html)
+    $('.image-large-area').css('display', 'block').fadeIn()
+  })
 
-  // クリックすると、クリックした画像とキャプションを読み込み、
-  // 作成した img要素の画像・キャプションとする
-  setClick = function (i) {
-	eles[i].addEventListener('click', function () {
-      body_area.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
-      image_area.style.display = 'block';  // 領域を表示
-      
-	  imgs = eles[i].getElementsByTagName('img'); // HtmlCollection
-      imgcap = eles[i].getElementsByTagName('figcaption'); // HtmlCollection
-      if (imgcap.length === 0) {
-        imgcap = [{textContent: ""}];
-      }
+});
 
-	  targetImg.setAttribute('src', imgs[0].src);
-	  targetImg.setAttribute('alt', imgcap[0].textContent);
-	  targetFigcap.textContent = imgcap[0].textContent;
-
-      // フェードイン
-      targetImg.animate (
-        [
-          {opacity: 0},
-          {opacity: 1}
-        ],
-        {
-          duration: 800,
-          fill: 'forwards'
-        }
-      );
-      setImageClick();
-	}, false);
-  };
-
-  // 初期設定
-  // ギャラリーの最初の画像とキャプションを読み込む
-  imgs = eles[0].getElementsByTagName('img');
-  imgcap = eles[0].getElementsByTagName('figcaption'); // HtmlCollection
-  if (imgcap.length === 0) {
-    imgcap = [{ textContent: "" }];
-  }
-  
-  // 作成した img要素を <figure> の最初の子要素とする
-  imageLargeEle.insertBefore(targetImg, imageLargeEle.firstChild);
-  // ギャラリーの最初の画像とキャプションに設定
-  targetImg.setAttribute('src', imgs[0].src);
-  targetImg.setAttribute('alt', imgcap[0].textContent);
-  targetFigcap.textContent = imgcap[0].textContent;
-
-  for (i = 0; i < eles.length; i = i + 1) {
-	setClick(i);
-  }
-})();
-
-// 修正時刻: Fri Aug  6 15:44:50 2021
+// 修正時刻: Fri Aug  6 22:15:28 2021
