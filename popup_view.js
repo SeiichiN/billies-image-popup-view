acd2851a90c1b36f5d445a7781b46fdbc8e5312e
SeiichiN <billie175@gmail.com>
2021-08-07 10:00:21 +0900

    doukana

--- popup_view.js
+++ popup_view.js
@@ -6,7 +6,7 @@ $( () => {
   const html_txt = String()
              + '<div class="image-large-area">'
              +   '<div class="image-area-bg">'
-             +     '<figure id="image-large">'
+             +     '<figure class="image-large">'
              +       '<figcaption id="figcap"></figcaption>'
              +     '</figure>'
              +   '</div>'
@@ -17,18 +17,28 @@ $( () => {
     const img_src = e.target.src
     const img_alt = e.target.alt
     const img_html = '<img src="' + img_src + '" alt="' + img_alt + '">'
-    $('#image-large').prepend(img_html)
+    $('.image-large').prepend(img_html)
+    // console.log($('.image-large')[0])
+    // console.log($('.image-large img')[0].height)
+    const height = $('.image-large img')[0].height + 20
+    const width = $('.image-large img')[0].width + 20
+    $('.image-area-bg').css('width', width)
+    $('.image-area-bg').css('height', height)
     $('.image-large-area').fadeIn()
-    return false;
+    setBgClick()
+    return false
   })
 
-  $('.image-area-bg').on('click', () => {
-    console.log('CLICK!')
-    $('.image-large-area').fadeOut()
-    $('.image-large-area').remove()
-    return false;
-  })
+  const setBgClick = () => {
+    $('.image-area-bg').on('click', () => {
+      $('.image-large-area').fadeOut()
+      setTimeout( () => {
+        $('.image-large-area').remove()
+      }, 800)
+      return false
+    })
+  }
 
-});
+})
 
-// 修正時刻: Fri Aug  6 23:00:31 2021
+// 修正時刻: Sat Aug  7 09:22:28 2021
